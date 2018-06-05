ae2bfb039d3d3411b5cc0c8ed4216d91ff248c94
dreamertn9527 <2712215541@qq.com>
2018-06-05 22:41:16 +0800

    新增代码

---
+++ design/src/main/java/dreamertn9527/top/App.java
@@ -0,0 +1,13 @@
+package dreamertn9527.top;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
