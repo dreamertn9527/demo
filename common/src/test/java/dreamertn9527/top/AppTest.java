51160177d8ff8716fffff20d796955cb97c733d5
dreamertn9527 <2712215541@qq.com>
2018-06-05 23:21:01 +0800

    新增代码

---
+++ common/src/test/java/dreamertn9527/top/AppTest.java
@@ -0,0 +1,20 @@
+package dreamertn9527.top;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
