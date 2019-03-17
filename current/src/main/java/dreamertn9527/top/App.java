974ac1692219e4849c4492767a24b4f59c358638
dreamertn9527 <2712215541@qq.com>
2019-03-17 23:37:28 +0800

    增加自定义线程

--- current/src/main/java/dreamertn9527/top/App.java
+++ current/src/main/java/dreamertn9527/top/App.java
@@ -1,5 +1,9 @@
 package dreamertn9527.top;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,14 @@ public class App
 {
     public static void main( String[] args )
     {
+        CountDownLatch countDownLatch = new CountDownLatch(10);
+        countDownLatch.countDown();
+        try {
+            countDownLatch.await();
+            ExecutorService executorService = Executors.newFixedThreadPool(11);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println( "Hello World!" );
     }
 }
